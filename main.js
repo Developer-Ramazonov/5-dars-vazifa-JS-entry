ded706345acedeb0523a1e3d3c8c5e68951c3d70
Developer-Ramazonov <ramazonovferuz01@yandex.com>
2021-12-24 14:51:01 +0300

    Updated

--- main.js
+++ main.js
@@ -1,5 +1,3 @@
-
-
 alert("Welcome our website !!!")
 
 alert('We are going to check you are robot or not');
@@ -7,9 +5,14 @@ alert('We are going to check you are robot or not');
 alert("You have to answer to our questions")
 
 var answer=prompt("Enter the right solution: 1 + 1 = ");
- if(answer==2)alert("Correct answer")
+ if(answer==2){
+     alert("Correct answer")
+    console.log('Do you want instruction')
+    }
  else {
      alert("Uncorrect answer \n There must be simple mistake, do you want another question?");
     answer=prompt("Enter the right solution: 2 + 5 = 7")
-    if(answer!=7)alert('We see that you are blind, you can not use our website !!!\nGood luck ...')
+    if(answer!=7)alert('It is clear thet you are not real. There is defend from robots, you can not use our website !!!\nGood luck ...')
 }
+
+
